804b86e18ebd7ec43363763010510657e1ed85bc
KDB <kirk.benell@sparkfun.com>
2019-06-25 14:06:34 -0600

    cleanup

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
 
     install_requires=setup_requires,
     # Choose your license
-    license='GPLv2',
+    license='MIT',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -73,12 +73,15 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both. 
-       'Programming Language :: Python :: 2.7'
-       'Programming Language :: Python :: 3'       
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+   
     ],
 
     # What does your project relate to?
