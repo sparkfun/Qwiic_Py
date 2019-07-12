96632210a2addad6a679458a3fa30758cf822aa7
KDB <kirk.benell@sparkfun.com>
2019-07-12 12:51:11 -0600

    version bump

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.9.3',
+    version='0.9.4',
 
     description='SparkFun Electronics qwiic library',
     long_description=long_description,
