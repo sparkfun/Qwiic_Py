693047e0e6e908444a88a79de988d5c54f08aea1
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2020-04-19 23:59:32 -0600

    Add ICM20948 module and bump version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.9',
+    version='1.0.10',
 
     description='SparkFun Electronics qwiic library',
     long_description=long_description,
