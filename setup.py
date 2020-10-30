da47545d1c831bfb4ce1f7a3fe05af0f822de3c4
Pete Lewis <pete.lewis@sparkfun.local>
2020-10-30 10:46:52 -0600

    bump again to v0.0.16

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.15',
+    version='1.0.16',
 
     description='SparkFun Electronics qwiic library',
     long_description=long_description,
