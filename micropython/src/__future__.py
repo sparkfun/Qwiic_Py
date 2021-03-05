2f255e92229bb4f3a769b7321885e00e49d6187f
oclyke <owen.lyke@sparkfun.com>
2021-03-05 15:14:51 -0700

    add stub of division

--- micropython/src/__future__.py
+++ micropython/src/__future__.py
@@ -2,3 +2,7 @@
 
 def print_function(*args):
   print(*args)
+
+def division(*args):
+  print(*args)
+  raise Exception('division is not implemented yet for micropython qwiic. please consider writing an implementation...')
