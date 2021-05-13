0b923c93b38bea51907b60ae4ed419f33bd5d2cc
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2021-05-13 13:07:27 -0600

    Fix list issues
    Fixes issue on how devices are listed as discussed in: https://github.com/sparkfun/Qwiic_Py/issues/2
    
    Implements changes that now lists devices as a "list of tuples"
    Modifies `getAvailableDevices`, `list_devices`, `get_devices`, and `create_device` methods to match new implementation
    
    Bump version

--- qwiic/__init__.py
+++ qwiic/__init__.py
@@ -4,7 +4,7 @@
 #------------------------------------------------------------------------
 #
 #
-# Written by  SparkFun Electronics, May 2019
+# Written by  SparkFun Electronics, May 2021
 #
 # This python library supports the SparkFun Electroncis qwiic
 # qwiic sensor/board ecosystem on a Raspberry Pi (and compatable) single
@@ -15,7 +15,7 @@
 # Do you like this library? Help support SparkFun. Buy a board!
 #
 #==================================================================================
-# Copyright (c) 2019 SparkFun Electronics
+# Copyright (c) 2021 SparkFun Electronics
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -118,7 +118,7 @@ class _QwiicInternal:
     # _getAvailableDevices()
     #
     # Return a dictionary that defines the available qwiic devices in the
-    # package. The key is the I2C address.
+    # package as a list. The key is the I2C address.
     #
     # Devices with multiple possible addresses have mulitple entries.
     @classmethod
@@ -132,11 +132,15 @@ class _QwiicInternal:
 
             for device_class in cls.qwiic_devices:
 
-                # loop over the device addresses - add name/address
+                # loop over the device addresses - add list of device name
                 for addr in device_class.available_addresses:
-                    cls.available_devices[addr] = device_class
+                    cls.available_devices.setdefault(addr,[]).append(device_class)
 
-        return cls.available_devices
+        # return cls.available_devices
+
+        # return cls.available_devices, sorted by the I2C addresses
+        return dict(sorted(cls.available_devices.items()))
+
 
 
 #----------------------------------------------------
@@ -238,17 +242,17 @@ def scan():
 #-----------------------------------------------------------------------
 # list_devices()
 #
-#   Return a list of tubles that define the qwiic devices connected to the
-#   I2C bus.
+#   Return a list of tuples that define the qwiic devices connected to the I2C bus.
 #
 def list_devices():
     """
-        Returns a list of known qwiic devices connected to the I2C bus.
+        Returns a list of known qwiic driver/packages for I2c address(es) of device(s) connected to the I2C bus.
 
-        :return: A list of known attached qwiic devices. If no devices are attached,
-            an empty list is returned.
-            Each element of the list a tuple that contains the following values
-            (Device I2C Address, Device Name, Device Driver Class Name)
+        :return: A list of the qwiic devices associated with the I2C address(es) 
+            scanned from the I2C bus. Each element of the list a tuple that 
+            contains the following values (Device I2C Address, Device Name, 
+            Device Driver Class Name)
+            If no devices are attached, an empty list is returned.
         :rtype: list
 
         :example:
@@ -256,15 +260,25 @@ def list_devices():
         >>> import qwiic
         >>> qwiic.list_devices()
         [(61, 'Qwiic Micro OLED', 'QwiicMicroOled'),
+        (61, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'),
+        (91, 'Qwiic PCA9685', 'QwiicPCA9685'),
+        (91, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'),
         (91, 'Qwiic CCS811', 'QwiicCcs811'),
+        (96, 'Qwiic PCA9685', 'QwiicPCA9685'),
+        (96, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'),
         (96, 'Qwiic Proximity Sensor', 'QwiicProximity'),
+        (119, 'Qwiic PCA9685', 'QwiicPCA9685'),
+        (119, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'),
+        (119, 'Qwiic Mux', 'QwiicTCA9548A'),
         (119, 'Qwiic BME280', 'QwiicBme280')]
+
     """
 
     # Scan the bus
-    found_devices = scan()
-    if not found_devices:
+    device_address = scan()
+    if not device_address:
         return []
+
 
     # What QWIIC devices do we know about -- what's defined in the package
     qwiic_devs = _QwiicInternal.get_available_devices()
@@ -273,12 +287,17 @@ def list_devices():
 
     found_qwiic = []
     # match scan, with definition
-    for address in found_devices:
+    for address in device_address:
 
         if address in qwiic_devs.keys():
-            # make the return tuple
-            found_qwiic.append((address, qwiic_devs[address].device_name, \
-                qwiic_devs[address].__name__))
+
+            # make list of devices at address
+            device_list = list(qwiic_devs[address])
+
+            for dev in device_list:
+                # make the return tuple
+                found_qwiic.append((address, dev.device_name, \
+                    dev.__name__))
 
 
     return found_qwiic
@@ -295,7 +314,7 @@ def get_devices():
         Used to create device objects for all qwiic devices attached to the computer.
 
         :return: A list of qwiic device objects.
-                 If no qwiic devices are an empty list is returned.
+            If no qwiic devices are an empty list is returned.
         :rtype: list
 
         :example:
@@ -326,9 +345,14 @@ def get_devices():
     for address in found_devices:
 
         if address in qwiic_devs.keys():
-            # Create an object and append to our found list
-            # note: class defs are stored in the defs dictionary..
-            found_qwiic.append(qwiic_devs[address]())
+
+            # make list of devices at address
+            device_list = list(qwiic_devs[address])
+
+            for dev in device_list:
+                # Create an object and append to our found list
+                # note: class defs are stored in the defs dictionary..
+                found_qwiic.append(dev(address))
 
 
     return found_qwiic
@@ -339,16 +363,21 @@ def get_devices():
 #   Given the Address, Name or Clasname of a qwiic device, create the
 #   assocaited device object and return it to the caller.
 #
-#   The intent is for the user to call list_devices(), find a device they like AND
-#   use this method to create the device
+#   The intent is for the user to call list_devices() (connected devices only),
+#   find a device they like AND use this method to create the device at the
+#   connected address
 #
 def create_device(device=None):
     """
         Used to create a device object for a specific qwiic device
 
-        :param device: The I2C address, Name or Class name of the device to created.
-        :return: A qwiic device object for the specified qwiic device.
-                 If the specified device isn't found, None is returned.
+        :param device: The I2C address (int), Name or Class name (str) of the device to created,
+            or selection from listed connected devices (tuple).
+            The device address should be an integer value, of the hex value. For
+            an example, for an `0x60` (hex) address enter `qwiic.create_device(60)`.
+        :return: A qwiic device object for the specified qwiic device, using the I2C address
+            that the device is connected at.
+            If the specified device isn't found, None is returned.
         :rtype: Object
 
         :example:
@@ -356,31 +385,68 @@ def create_device(device=None):
         >>> import qwiic
         >>> results = qwiic.list_devices()
         >>> print(results)
-        [(61, 'Qwiic Micro OLED', 'QwiicMicroOled'), (91, 'Qwiic CCS811', 'QwiicCcs811'),
-        (96, 'Qwiic Proximity Sensor', 'QwiicProximity'), (119, 'Qwiic BME280', 'QwiicBme280')]
-
-        >>> mydevice = qwiic.create_device(results[0][0])
-        >>> print(mydevice)
-        <qwiic_micro_oled.qwiic_micro_oled.QwiicMicroOled object at 0x751fdab0>
+        [(16, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'), (16, 'Qwiic Titan GPS', 'QwiicTitanGps'), (41, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'), (60, 'Qwiic Micro OLED (64x48)', 'QwiicMicroOled'), (60, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'), (60, 'SSD1306 Display Driver', 'QwiicSSD1306Driver'), (60, 'Qwiic OLED Display (128x32)', 'QwiicOledDisplay'), (61, 'Qwiic Micro OLED (64x48)', 'QwiicMicroOled'), (61, 'Qwiic 4m Distance Sensor (ToF)', 'QwiicVL53L1X'), (61, 'SSD1306 Display Driver', 'QwiicSSD1306Driver'), (61, 'Qwiic OLED Display (128x32)', 'QwiicOledDisplay')]
+        >>> mydevice1 = qwiic.create_device(results[47])
+        >>> print(mydevice1)
+        <qwiic_micro_oled.qwiic_micro_oled.QwiicMicroOled object at 0x743558f0>
+        >>> mydevice2 = qwiic.create_device(41)
+        >>> print(mydevice2)
+        <qwiic_vl53l1x.QwiicVL53L1X object at 0x743e7bb0>
+        >>> mydevice3 = qwiic.create_device("QwiicTitanGps")
+        ============================================================================
+        Message about Titan GPS package...
+        ============================================================================
+        >>> print(mydevice3)
+        <qwiic_titan_gps.QwiicTitanGps object at 0x739c4430>
 
     """
+
     if device is None:
         print("No device provided.")
         return None
 
     conn_devices = list_devices()
 
-    for curr_dev in conn_devices:
-        # the entries in the connDevices list are tuples, with the following values
-        #   (I2C address, Device Name, Class Name)
-        #
-        # If the provided value is in the current device tuple, we're done
-        if device in curr_dev:
-            # we need the class definition
-            qwiic_defs = _QwiicInternal.get_available_devices()
 
-            # Index using the I2C address (entry 0 of currDev)
-            return qwiic_defs[curr_dev[0]]()
+    # The entries in the connDevices list are tuples, with the values:
+    #   (I2C address, Device Name, Class Name)
+    # For the provided value (device), if a match is found in the current
+    # device tuple, a list is created from the tuple 
+    if type(device) is tuple:
+        dev_match = [curr_dev for curr_dev in conn_devices if device == curr_dev]
+    else:
+        dev_match = [curr_dev for curr_dev in conn_devices if device in curr_dev]
+
+
+    # If there are no matching entries for the inputted device descriptor
+    if len(dev_match) == 0:
+        print("Error: No matching connected devices\n")
+
+        return None
+
+    # If there are mulitple entries for the inputted device descriptor
+    elif len(dev_match) > 1:
+        # Print out matched devices
+        print("Error: Multiple matches found for input: \n")
+        print(dev_match, "\n")
+
+        return None
+
+    # If there is a match
+    else:
+        # we need the class definition
+        qwiic_defs = _QwiicInternal.get_available_devices()
+
+        
+        if type(device) is int:
+            return qwiic_defs[device][0](int)
+        else:
+            # Match classes that are indexed at dev_address, to the dev_name
+            for dev_class in qwiic_defs[dev_match[0][0]]:
+                if dev_class.device_name == dev_match[0][1]:
+                    # Create a device using cless at the connected address
+                    return dev_class(dev_match[0][0])
+
 
     # if we are here, we have an issue
     print("Unabled to create requested device - is the device connected?")
